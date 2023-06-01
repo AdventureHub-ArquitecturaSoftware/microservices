bf05562a6e6c774d13a29f484e1c7fe21f4d0854
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-01 14:10:01 -0500

    fix: fixed modules-microservices with correct configuration.

---
+++ microservices/reservation-composite-service/src/main/java/com/example/reservationcompositeservice/ReservationCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationcompositeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationCompositeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationCompositeServiceApplication.class, args);
+    }
+
+}
