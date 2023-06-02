e6961228160c5d110fa89bfc95c8f5298bf762ec
Jared <u202020239@upc.edu.pe>
2023-06-01 22:54:19 -0500

    feat(fixed) Fixed Payment Mapping
    Delete loombock and added parameters manuality

--- api/src/main/java/com/adventurehub/api/core/payment/Payment.java
+++ api/src/main/java/com/adventurehub/api/core/payment/Payment.java
@@ -1,5 +1,6 @@
 package com.adventurehub.api.core.payment;
 
+import com.adventurehub.api.composite.travelPackage.ServiceAddresses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,6 @@ import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class Payment {
     private Integer userId;
     private Integer reservationId;
@@ -19,4 +18,30 @@ public class Payment {
     private Date paymentDate;
     private String paymentMethod;
     private String serviceAddress;
+
+
+    public Integer getUserId(){return userId;}
+    public void setUserId(Integer userId){this.userId=userId;}
+
+    public Integer getReservationId(){return reservationId;}
+    public void setReservationId(Integer reservationId){this.reservationId=reservationId;}
+
+    public Double getAmount(){return amount;}
+    public void setAmount(Double amount){this.amount=amount;}
+
+
+    public Date getPaymentDate(){return paymentDate;}
+    public void setPaymentDate(Date paymentDate){this.paymentDate=paymentDate;}
+
+    public Integer getPaymentId(){return paymentId;}
+    public void setPaymentId(Integer paymentId){this.paymentId=paymentId;}
+
+    public String getPaymentMethod(){return paymentMethod;}
+    public void setPaymentMethod(String paymentMethod){this.paymentMethod=paymentMethod;}
+
+    public String getServiceAddress(){return serviceAddress;}
+    public void setServiceAddress(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
+    }
+
 }
